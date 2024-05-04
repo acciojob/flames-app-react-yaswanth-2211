5d86260101fdb4763433d69be3180de3776dd478
unknown <kavaliyaswanth2@gmail.com>
2024-05-04 17:35:32 +0530

    submitting code-Sat May 04 2024 17:35:31 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,50 @@
-import React, {Component, useState} from "react";
+import React, {useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
-
+const App=()=>{
+    const[name1,setName1]=useState('')
+    const[name2,setName2]=useState('')
+    const[relation,setRelation]=useState('')
+    let relations=["Friends","Love","Affection","Marriage","Enemy","Siblings"]
+    const calculate=()=>{
+        setRelation(findR(name1,name2));
+    }
+    const findR =()=>{
+        let map =new Map();
+        let n=name1.length;
+        let m=name2.length;
+        for(let i=0;i<n;i++){
+            let char=name1[i];
+            if(map[i]){
+                map[i]++;
+            }
+            else{
+                map[char]=1;
+            }
+        }
+        let common=0;
+        for(let i=0;i<m;i++){
+            let char=name2[i];
+            if(map[i]){
+                map[i]--;
+                common++
+            }
+        }
+        return(relations[(n+m-(2*common))%6]);
+    }
+    const clear=()=>{
+        setName1("");
+        setName2("");
+        setRelation("");
+    }
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               <input value={name1} onChange={(e)=>setName1(e.target.value)} data-testid="input1"/>
+               <input value={name2} onChange={(e)=>setName2(e.target.value)} data-testid="input2"/>
+               <button onClick={calculate} data-testid="calculate_relationship">Calculate Relationship Future</button>
+               <button onClick={clear} data-testid="clear">Clear</button>
+               <h1 data-testid="answer">{relation}</h1>
             </div>
         )
     }
-}
-
-
 export default App;
